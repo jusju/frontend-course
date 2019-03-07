f8884b3e4925ffe7292131bb8b8bc885d77b1e06
Juslin Jukka <jusju@haaga-helia.fi>
2019-03-07 10:33:31 +0200

    router

--- react/react-router/src/App.js
+++ react/react-router/src/App.js
@@ -1,28 +1,51 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Link, Switch, Route }  from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <BrowserRouter>
+          <div>
+            <Link to="/"  > Home</Link>{' '}
+            <Link to="/about">About</Link>{' '}
+            <Link to="/contact">Contact</Link>{' '}
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/contact"
+                render={() => <h1>Contact address</h1>} />
+              <Route render={() => <h1>Page not found</h1>} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+
       </div>
     );
   }
 }
+
+export class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Home</h1>
+      </div>
+    );
+  }
+}
+
+export class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+      </div>
+    );
+  }
+}
+
 
 export default App;
